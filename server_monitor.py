d2e5d64aac30304e44a79a18528881f7aa37e2fb
Slaykus <63318050+Slaykus@users.noreply.github.com>
2020-07-29 15:55:54 +0300

    Update server_monitor.py

--- server_monitor.py
+++ server_monitor.py
@@ -16,6 +16,6 @@ with cpu:
         print("Температура процессора - " + cpu.temperature + "Градусов")
         sleep(1)
 while True:
-    api.messages.send(peer_id = 282902478, message = "Температура процессора - " + cpu.temperature + "Градусов")
+    api.messages.send(peer_id = 282902478, message = "Температура процессора - " + str(cpu.temperature) + "Градусов")
     time.sleep(15)
     
